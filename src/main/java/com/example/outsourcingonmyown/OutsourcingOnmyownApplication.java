ce76b05e584ba9d6589e4673df127be623937615
gkdl4239 <gkdl4239@gmail.com>
2025-01-21 13:56:55 +0900

    init

---
+++ src/main/java/com/example/outsourcingonmyown/OutsourcingOnmyownApplication.java
@@ -0,0 +1,13 @@
+package com.example.outsourcingonmyown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutsourcingOnmyownApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutsourcingOnmyownApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/outsourcingonmyown/OutsourcingOnmyownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.outsourcingonmyown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutsourcingOnmyownApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
